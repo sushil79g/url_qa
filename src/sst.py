23cf0ec35e6110f45300473b793fec9ac964209e
Anish Pandey <sharmaanix@hotmail.com>
2021-01-08 15:50:02 +0545

    Add default text filename rather giving text directory

--- src/sst.py
+++ src/sst.py
@@ -39,30 +39,32 @@ def predict(input_batch, model, decoder):
 def download_wav_file(video_url, video_dir):
     yt = YouTube(video_url)
     old_filename = os.path.join(video_dir, yt.streams.first().default_filename)
-    new_filename = re.sub(r'.mp4', '', old_filename)+str(".wav")
-    if not os.path.exists(new_filename):
+    wav_filename = re.sub(r'.mp4', '', old_filename)+str(".wav")
+    text_filename = re.sub(r'.mp4', '', old_filename)+str(".txt")
+    if not os.path.exists(wav_filename):
         tube_wav = yt.streams.filter(only_audio=True, file_extension='mp4')
         tube_wav[0].download(video_dir)
-        subprocess.call(["ffmpeg","-i", old_filename, new_filename])
+        subprocess.call(["ffmpeg","-i", old_filename, wav_filename])
         subprocess.call(["rm","-rf", old_filename])
-    return new_filename
+    return wav_filename, text_filename
 
     
 
 
-def extract_text(model_dir, model_name, video_url, video_dir,link_id,text_dir):
-    if not os.path.exists(text_dir):
-        model, decoder, device = load_sst_model(model_dir, model_name, link_id)
-        new_filepath = download_wav_file(video_url, video_dir)
-        input_set = prepare_model_input(read_batch([new_filepath]),device=device)
+def extract_text(model_dir, model_name, video_url, video_dir,link_id):
+    
+    model, decoder, device = load_sst_model(model_dir, model_name, link_id)
+    wav_filepath, text_filepath = download_wav_file(video_url, video_dir)    
+    if not os.path.exists(text_filepath):
+        input_set = prepare_model_input(read_batch([wav_filepath]),device=device)
         dmodel = dask.delayed(model.cpu())
         predictions = [predict(input_set, dmodel, decoder)]
         text = dask.compute(*predictions)[0]
-        file_txt = open(text_dir,"w")
+        file_txt = open(text_filepath,"w")
         file_txt.write(text)
         file_txt.close()
     else:
-        file_txt = open(text_dir,"r")
+        file_txt = open(text_filepath,"r")
         text = file_txt.read()
 
     return text
